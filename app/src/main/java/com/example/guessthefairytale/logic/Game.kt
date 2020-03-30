71d334d0bf39ee571966c66e6bae5ad31b0ae2de
Ula <ulam98@gmail.com>
2020-03-30 15:12:19 +0200

    Code refactor, changed names, added empty lines, changed initializeRoundSong() method

--- app/src/main/java/com/example/guessthefairytale/logic/Game.kt
+++ app/src/main/java/com/example/guessthefairytale/logic/Game.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import kotlin.random.Random
 
 class Game {
-    private var roundsNumber: Int = 4
+    private var roundsNumber: Int = 0
     private var roundTime: Int = 30000
     private var breakTime: Int = 3000
     private var score: Double = 0.0
@@ -28,17 +28,17 @@ class Game {
         return actualSong
     }
 
-    fun setRoundsNumber(number:Int){
-        roundsNumber=number
+    fun setRoundsNumber(number: Int) {
+        roundsNumber = number
     }
 
-    fun addPoints(points:Double) {
+    fun addPoints(points: Double) {
         if (score < roundsNumber) {
             score += points
         }
     }
 
-    fun initializeLib(context: Context){
+    fun initializeLib(context: Context) {
         songsLib.readLibraryFromFile(context)
     }
 
@@ -48,25 +48,28 @@ class Game {
     }
 
     private fun initializeRoundSong() {
+        var index = 0
+
         while (actualSong.wasPlayed()) {
-            val index = Random.nextInt(0, songsLib.libSize)
+            index = Random.nextInt(0, songsLib.libSize)
             val song = songsLib.getSongsList()[index]
             if (!song.wasPlayed()) {
                 actualSong = song
             }
         }
-        songsLib.getSongsList()[songsLib.getSongsList().indexOf(actualSong)].played()
+        songsLib.getSongsList()[index].played()
     }
 
     private fun getAnswers(): ArrayList<String> {
         val result: ArrayList<String> = arrayListOf()
+
         while (result.size < answersNumber) {
             val song = songsLib.getSongsList()[Random.nextInt(0, songsLib.libSize)]
             if (!result.contains(song.getFairyTale()) && !song.wasPlayed())
                 result.add(song.getFairyTale())
         }
-        result[Random.nextInt(0, answersNumber)] = actualSong.getFairyTale()
 
+        result[Random.nextInt(0, answersNumber)] = actualSong.getFairyTale()
         return result
     }
 }
