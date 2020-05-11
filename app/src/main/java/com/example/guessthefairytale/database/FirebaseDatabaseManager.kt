291d059a8069cce7446e50feaaed94e5e8d86df7
Ula <ulam98@gmail.com>
2020-05-11 21:54:52 +0200

    cleanup

--- app/src/main/java/com/example/guessthefairytale/database/FirebaseDatabaseManager.kt
+++ app/src/main/java/com/example/guessthefairytale/database/FirebaseDatabaseManager.kt
@@ -5,25 +5,21 @@ import com.example.guessthefairytale.database.dto.User
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-
-
 class FirebaseDatabaseManager {
-    val database = Firebase.database
+    private val database = Firebase.database
     private val KEY_USER = "user"
     private val KEY_GAME = "game"
 
     fun createUser(id: String, name: String, email: String) {
         val user = User(id, name, email, 0)
         val res = database
-            .reference        // 1
-            .child(KEY_USER)  // 2
-            .child(id)        // 3
-            .child("username").setValue(name)   // 4
+            .reference
+            .child(KEY_USER)
+            .child(id).setValue(user)
         Log.e("result: ", res.result.toString())
     }
 
-    fun addReference(){
-
+    fun addReference() {
         database.reference.child(KEY_USER)
     }
 }
