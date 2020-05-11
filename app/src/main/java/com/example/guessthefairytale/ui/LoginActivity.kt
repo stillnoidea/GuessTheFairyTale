bfa7ad4946fd55b47fea9092821afd71f9541eb6
Ula <ulam98@gmail.com>
2020-05-11 21:57:45 +0200

    Tried to add functionality, that adds new user to database after registration, and added functionality that checks if user is logged after app start if so, the step of logging is overlooked

--- app/src/main/java/com/example/guessthefairytale/ui/LoginActivity.kt
+++ app/src/main/java/com/example/guessthefairytale/ui/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guessthefairytale.R
+import com.example.guessthefairytale.database.FirebaseDatabaseManager
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -21,16 +22,23 @@ class LoginActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("user", currentUser)
+            startActivity(intent)
+        }
+
         login_activity_signin_button.setOnClickListener {
             startSigningIn()
         }
+
     }
 
-    fun startSigningIn() {
+    private fun startSigningIn() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build()
         )
-
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
@@ -50,6 +58,10 @@ class LoginActivity : AppCompatActivity() {
                 resultCode == Activity.RESULT_OK -> {
 
                     val user = FirebaseAuth.getInstance().currentUser
+                    if (response!!.isNewUser) {
+                        Toast.makeText(this, user!!.displayName, Toast.LENGTH_LONG).show()
+                        FirebaseDatabaseManager().createUser(user.uid, user.displayName!!, user.email!!)
+                    }
 
                     val intent = Intent(this, MainActivity::class.java)
                     intent.putExtra("user", user)
