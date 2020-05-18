fcbe08bf8b163c2dccefe4169aa10d282481bce7
Ula <samaniewiem>
2020-05-19 00:13:23 +0200

    Added Serializable, so it easier to send this data throw intent

--- app/src/main/java/com/example/guessthefairytale/database/dto/User.kt
+++ app/src/main/java/com/example/guessthefairytale/database/dto/User.kt
@@ -1,6 +1,10 @@
 package com.example.guessthefairytale.database.dto
 
-data class User(val id: String,
-                val username: String,
-                val email: String,
-                val score: Int)
+import java.io.Serializable
+
+data class User(
+    val id: String,
+    val username: String,
+    val email: String,
+    val score: Int
+) : Serializable
