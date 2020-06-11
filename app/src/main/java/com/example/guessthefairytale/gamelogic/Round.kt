896c05fb7f8da866bf8f3190ec9580afeeb8f33e
Ula <samaniewiem>
2020-06-11 23:50:50 +0200

    Added round data class

---
+++ app/src/main/java/com/example/guessthefairytale/gamelogic/Round.kt
@@ -0,0 +1,7 @@
+package com.example.guessthefairytale.gamelogic
+
+class Round{
+    var correctAnswer: Int = 0
+    lateinit var answersList: ArrayList<String>
+    lateinit var currentSong: Song
+}
