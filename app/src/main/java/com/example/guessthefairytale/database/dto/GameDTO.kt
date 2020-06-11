62f3b8686b2af81c52d7c9bada49ab50ddccdfc5
Ula <samaniewiem>
2020-06-11 23:47:20 +0200

    Changed name added new fields

---
+++ app/src/main/java/com/example/guessthefairytale/database/dto/GameDTO.kt
@@ -0,0 +1,15 @@
+package com.example.guessthefairytale.database.dto
+
+import com.example.guessthefairytale.gamelogic.Round
+
+data class GameDTO(
+    val roundsNo: Int,
+    var actualRound: Int,
+    var rounds: List<Round>,
+    var p1Score: Int,
+    var p2Score: Int,
+    var p1Ready: Boolean,
+    var p2Ready: Boolean
+) {
+    constructor() : this(0, 0, arrayListOf<Round>(), 0, 0, false, false)
+}
